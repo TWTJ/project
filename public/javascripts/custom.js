cb721552892693d7d0ff898413b0dc53bac92dbf
Kim Yoonji <goode89@naver.com>
2019-10-21 17:43:59 +0900

    revise main page real
    revise main page real

---
+++ public/javascripts/custom.js
@@ -0,0 +1,40 @@
+(function ($) {
+    'use strict';
+
+    $(document).on('ready', function () {
+        // -----------------------------
+        //  Screenshot Slider
+        // -----------------------------
+        $('.speaker-slider').slick({
+            slidesToShow: 3,
+            centerMode: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed:3000,
+            arrows:true,
+            dots:true,
+            fade:false,  
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+         });      
+    });
+
+})(jQuery);
