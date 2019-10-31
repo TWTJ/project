c774df592feb600d388c6b4bc3f7b77826346a47
rockjeon <sangwook27@naver.com>
2019-10-31 10:22:29 +0900

    TWTJ

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./models'); // db 모듈 불러오기
+var bodyParser  = require('body-parser'); //데이터 처리 미들웨어
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mainRouter = require('./routes/main');
+var PAGEAPI = require('./routes/page');
+var STROEAPI = require('./routes/store');
+var USERAPI = require('./routes/user');
 var app = express();
+
+//db 연결
+db();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,21 +20,26 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mainRouter);
-app.use('/users', usersRouter);
-app.use('/index', indexRouter);
+app.use('/', PAGEAPI); 
+app.use('/API_STORE', STROEAPI);
+app.use('/API_USER', USERAPI);
+app.use('/signin', PAGEAPI);
+app.use('/signup', PAGEAPI);
+// app.use('/sign_up', PAGEAPI);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,3 +50,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
---
+++ routes/user/user.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const route = express.Router()
+const users = require('../../models/users')
+// const pbkdf2Password = require('pbkdf2-password');
+// const hasher = pabkdf2Password();
+
+// get user info
+route.get('/', (req, res) => {
+    return res.status(200).json({
+        msg: 'get user info'
+    })
+})
+
+// regist user info
+route.post('/sign_up', (req, res) => {
+    let body = req.body;
+    console.log(body)
+    users.create({
+        name: body.user_name,
+        Id: body.user_Id,
+        password: body.user_pw,
+        email: body.user_email
+    })
+        .then(result => {
+            res.redirect('/signin')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
+})
+
+// user login
+route.post('/signin', (req, res) => {
+    return res.status(201).json({
+        msg: 'user signin'
+    })
+})
+
+
+module.exports = route
